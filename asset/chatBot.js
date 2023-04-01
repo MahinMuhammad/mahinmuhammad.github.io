0e25047c4ca16814e4691cd468354c3858159c83
MahinMuhammad <amimahinurrahman@gmail.com>
2023-04-01 16:06:47 +0600

    chat bot openning and closing

--- asset/chatBot.js
+++ asset/chatBot.js
@@ -1,3 +1,11 @@
 function iconClicked(){
-    alert("Module under development!");
+    if(document.getElementById("chatBotBox").style.display == "none" || document.getElementById("chatBotBox").style.display == ""){
+        document.getElementById("chatBotBox").style.display = "block";
+        document.getElementById("chatBotIcon").innerHTML = '<img src="../asset/minimizeIcon.png"></img>';
+    } 
+    // == "" because I don't know why first time it gets "" as display type
+    else{
+        document.getElementById("chatBotBox").style.display = "none";
+        document.getElementById("chatBotIcon").innerHTML = '<img src="../asset/chatbot.png"></img>';
+    }
 }
