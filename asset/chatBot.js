c0e9bd2fc6b70efeb43cc7ac10b27aac73d8b2a8
MahinMuhammad <amimahinurrahman@gmail.com>
2023-04-01 17:54:26 +0600

    chat bot ready for python ai

--- asset/chatBot.js
+++ asset/chatBot.js
@@ -1,11 +1,27 @@
 function iconClicked(){
     if(document.getElementById("chatBotBox").style.display == "none" || document.getElementById("chatBotBox").style.display == ""){
         document.getElementById("chatBotBox").style.display = "block";
+        setTimeout(() => {
+            document.getElementById("chatBotBox").style.opacity = "1";
+        });
         document.getElementById("chatBotIcon").innerHTML = '<img src="../asset/minimizeIcon.png"></img>';
     } 
     // == "" because I don't know why first time it gets "" as display type
     else{
-        document.getElementById("chatBotBox").style.display = "none";
-        document.getElementById("chatBotIcon").innerHTML = '<img src="../asset/chatbot.png"></img>';
+        setTimeout(() => {
+            document.getElementById("chatBotBox").style.opacity = "0";
+        });
+        setTimeout(() => {
+            document.getElementById("chatBotBox").style.display = "none";
+        }, 500);
+        setTimeout(() => {
+            document.getElementById("chatBotIcon").innerHTML = '<img src="../asset/chatbot.png"></img>';
+        }, 200);
+    }
+}
+
+function submitChat(event){
+    if(event.code == "Enter"){
+        alert("Chat bot is sleeping..Zzzz")
     }
 }
